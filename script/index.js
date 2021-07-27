82a1c32accd452057c09127d13bbe645b2f4e58d
reemsh2000 <sh951652@gmail.com>
2021-07-27 22:15:08 +0300

    final result in js Relates #17

--- script/index.js
+++ script/index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const cards = document.querySelectorAll(".container");
 function generateElement(tag, className, parentNode) {
   const tagName = document.createElement(tag);
@@ -25,3 +26,59 @@ function createCards(url, title, category, recipe, videoLink, sectionNum) {
     videoIcon.classList.add("fa-youtube");
   }
 }
+=======
+let request = (url, callback) => {
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const data = JSON.parse(xhr.responseText);
+      callback(data);
+    }
+  };
+  xhr.open("GET", url);
+  xhr.send();
+};
+
+let displayData = () => {
+  const submit = document.querySelectorAll(".search-btn");
+  const input = document.querySelectorAll(".food-input");
+  submit.forEach((ele, index) => {
+    ele.addEventListener("click", () => {
+      if (index === 0) {
+        request(
+          `https://www.themealdb.com/api/json/v1/1/search.php?s=${input[index].value}`,
+          (data) => {
+            for (let i = 0; i < 6; i++) {
+              createCards(
+                data.meals[i].strMealThumb,
+                data.meals[i].strMeal,
+                data.meals[i].strCategory,
+                data.meals[i].strInstructions,
+                data.meals[i].strYoutube,
+                0
+              );
+            }
+          }
+        );
+      } else {
+        request(
+          `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input[index].value}`,
+          (data) => {
+            for (let i = 0; i < 6; i++) {
+              createCards(
+                data.drinks[i].strDrinkThumb,
+                data.drinks[i].strDrink,
+                data.drinks[i].strCategory,
+                data.drinks[i].strInstructions,
+                "",
+                1
+              );
+            }
+          }
+        );
+      }
+    });
+  });
+};
+displayData();
+>>>>>>> Stashed changes
