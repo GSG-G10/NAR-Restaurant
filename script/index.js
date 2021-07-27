c06c5021a23621007216de31ba20c05d35b0c0ab
Raghad-Mezied <rmezied3@gmail.com>
2021-07-27 19:30:22 +0300

    create functions to create cards relates #7

---
+++ script/index.js
@@ -0,0 +1,27 @@
+const cards = document.querySelectorAll(".container");
+function generateElement(tag, className, parentNode) {
+  const tagName = document.createElement(tag);
+  tagName.classList.add(className);
+  parentNode.appendChild(tagName);
+  return tagName;
+}
+function createCards(url, title, category, recipe, videoLink, sectionNum) {
+  const card = generateElement("div", "card", cards[sectionNum]);
+  const imgContainer = generateElement("div", "img-container", card);
+  const cardImg = generateElement("img", "card-img", imgContainer);
+  cardImg.src = url;
+  cardImg.alt = `${title} ${category}`;
+  const detailsContainer = generateElement("div", "details-container", card);
+  const cardName = generateElement("p", "card-name", detailsContainer);
+  cardName.textContent = title;
+  const cardCategory = generateElement("p", "card-category", detailsContainer);
+  cardCategory.textContent = category;
+  const cardRecipe = generateElement("p", "card-recipe", detailsContainer);
+  cardRecipe.textContent = recipe;
+  if (sectionNum == 0) {
+    const cardVideo = generateElement("a", "card-video", detailsContainer);
+    cardVideo.href = videoLink;
+    const videoIcon = generateElement("i", "fab", cardVideo);
+    videoIcon.classList.add("fa-youtube");
+  }
+}
